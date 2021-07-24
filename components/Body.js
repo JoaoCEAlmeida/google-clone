36bf020e805a477e2be24291ae4ce9e7d4d749da
johnnygato <joao88vsc@gmail.com>
2021-07-24 15:52:49 +0100

    build search functionality

--- components/Body.js
+++ components/Body.js
@@ -1,8 +1,25 @@
+import { useRef } from "react";
+
 import Image from "next/image";
+import { useRouter } from "next/dist/client/router";
+
 import { SearchIcon } from "@heroicons/react/outline";
 import { MicrophoneIcon } from "@heroicons/react/solid";
 
 const Body = () => {
+  const router = useRouter();
+
+  const searchInputRef = useRef(null);
+  const search = (e) => {
+    e.preventDefault(); //prevent page reload
+
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  };
+
   return (
     <form className="flex flex-col items-center mt-44 flex-grow px-2">
       <Image
@@ -13,6 +30,7 @@ const Body = () => {
       <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
         <SearchIcon className="h-5 mr-3 text-gray-500" />
         <input
+          ref={searchInputRef}
           type="text"
           placeholder="Search"
           className="flex-grow px-5 text-base bg-transparent outline-none"
@@ -20,8 +38,12 @@ const Body = () => {
         <MicrophoneIcon className="h-5 mr-3 text-gray-500" />
       </div>
       <div className="flex flex-col sm:flex-row  w-1/2 space-y-2 sm:space-y-0 justify-center mt-8 mb-8 sm:space-x-4">
-        <button className="button">Google Search</button>
-        <button className="button">I'm feeling lucky</button>
+        <button onClick={search} className="button">
+          Google Search
+        </button>
+        <button onClick={search} className="button">
+          I'm feeling lucky
+        </button>
       </div>
     </form>
   );
