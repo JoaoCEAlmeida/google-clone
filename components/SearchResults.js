958d635735fb7b9effbbf2be2ced81f38abb7041
johnnygato <joao88vsc@gmail.com>
2021-07-24 17:55:21 +0100

    build search results layout and functionality with ssr

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,7 +1,33 @@
-const SearchResults = () => {
+import PaginationButtons from "./PaginationButtons";
+const SearchResults = ({ results }) => {
   return (
-    <div>
-      <p>Results</p>
+    <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
+      {!results?.error ? (
+        <>
+          <p className="text-gray-600 text-md mb-5 mt-3">
+            About {results.searchInformation?.formattedTotalResults} results (
+            {results.searchInformation?.formattedSearchTime} seconds)
+          </p>
+          {results?.items?.map((res) => (
+            <div key={res.link} className="max-w-xl mb-8">
+              <div className="group">
+                <a href={res.link} className="line-clamp-1 text-sm">
+                  {res.formattedUrl}
+                </a>
+                <a href={res.link} className="text-sm">
+                  <h2 className="line-clamp-1 text-xl text-blue-800 font-medium group-hover:underline">
+                    {res.title}
+                  </h2>
+                </a>
+              </div>
+              <p className="line-clamp-2">{res.snippet}</p>
+            </div>
+          ))}
+          <PaginationButtons />
+        </>
+      ) : (
+        <p className="text-gray-600 text-md mb-5 mt-3">No Results Found</p>
+      )}
     </div>
   );
 };
--- pages/search.js
+++ pages/search.js
@@ -1,19 +1,39 @@
 import Head from "next/head";
 import SearchHeader from "../components/SearchHeader";
 import SearchResults from "../components/SearchResults";
+import Response from "../Response";
+import { useRouter } from "next/router";
 
-const Search = () => {
+const Search = ({ results }) => {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google Search</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
 
       <SearchHeader />
-      <SearchResults />
+      <SearchResults results={results} />
     </div>
   );
 };
 
 export default Search;
+
+export async function getServerSideProps(context) {
+  const startIndex = context.query.start || "0";
+
+  //API_KEY - https://developers.google.com/custom-search/v1/using_rest
+  //CONTEXT_KEY - https://cse.google.com/cse/create/new
+  const data = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: data,
+    },
+  };
+}
