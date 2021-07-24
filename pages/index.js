9759dd9334f701d95d2665d2058429987094b13f
johnnygato <joao88vsc@gmail.com>
2021-07-24 14:09:44 +0100

    setup project

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Google Clone</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+    </div>
+  );
+}
